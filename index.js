09d8a3406b0a72611e57ad0f312c760ea1091344
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2019-03-04 12:20:32 +0100

    added version

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const { version } = require("./package.json");
 
-app.get('/', (req, res) => res.send('Hello k8s!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello k8s!"));
+app.get("/version", (req, res) => res.send(version));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
